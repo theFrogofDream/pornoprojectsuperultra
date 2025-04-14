4f9d6627a4f14a9297e119b6b444ce10e1049662
thrFrogofDream <al.kov2006@gmail.com>
2025-04-14 12:30:27 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+        <iframe src="https://mamapornovideo.org" width="650" height="600" scrolling="auto"></iframe>
+      </>
+  )
+}
+
+export default App
